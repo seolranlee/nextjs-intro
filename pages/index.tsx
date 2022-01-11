b10dc2c29dc6408f5f2a9cf852f4bd31103632a0
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2022-01-11 16:16:35 +0900

    #2.1 Fetching Data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,45 @@
-import Head from "next/head";
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "37650be0d67abada8cd0f17f654ce2c9"
+
+export interface Movie {
+  "adult": boolean;
+  "backdrop_path": string;
+  "genre_ids": number[];
+  "id": number;
+  "original_language": string;
+  "original_title": string;
+  "overview": string;
+  "popularity": number;
+  "poster_path": string;
+  "release_date": string;
+  "title": string;
+  "video": boolean;
+  "vote_average": number;
+  "vote_count": number
+}
+
 export default function Home(){
+  const [movies, setMovies] = useState<null|Movie[]>()
+  useEffect(() => {
+    (async() => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json()
+      setMovies(results)
+      console.log(results)
+
+    })()
+
+  }, [])
   return (
     <div>
       <Seo title="Home" />
-      <h1 className="active">Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map(movie => <div key={movie.id}><h4>{movie.original_title}</h4></div>)}
     </div>
   )
 }
