50e8d11edbc683083a7536d6935605725c36d1b5
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2022-01-11 15:02:13 +0900

    #1.7 Recap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 // NextJS에서 앱을 만들 때 모든 앱의 청사진이 되는 지점
 // NetJS가 보는 순서
 // 1. _app => 2. index
+// 1. _app => 2. about
+// 어떤 페이지를 렌더링하던지 항상 _app을 먼저 거쳐간다.
 
 import NavBar from "../components/NavBar";
 // Global CSS cannot be imported from files other than your Custom <App>. 
