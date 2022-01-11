1abb8c3e500ab0cbf7c126ccccbe8855c1a16187
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2022-01-11 17:38:33 +0900

    #2.2 Redirect and Rewrite

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = process.env.API_KEY
+
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      }
+    ]
+  },
+  // rewirtes: 유저를 redirect시키지만 url은 변하지 않는다.
+  // NexJS가 request를 masking 해줄 수 있다.
+  // request 탭에서 API KEY가 노출되지 않는다.
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      }
+    ]
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-const API_KEY = "37650be0d67abada8cd0f17f654ce2c9"
-
 export interface Movie {
   "adult": boolean;
   "backdrop_path": string;
@@ -24,22 +22,43 @@ export default function Home(){
   const [movies, setMovies] = useState<null|Movie[]>()
   useEffect(() => {
     (async() => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json()
+      // NexJS가 request를 masking 해줄 수 있다.
+      // /api/movies: fake fetching url
+      const { results } = await (await fetch("/api/movies")).json()
       setMovies(results)
-      console.log(results)
-
     })()
-
   }, [])
   return (
-    <div>
+    <div className="container">
       <Seo title="Home" />
       {!movies && <h4>Loading...</h4>}
-      {movies?.map(movie => <div key={movie.id}><h4>{movie.original_title}</h4></div>)}
+      {movies?.map((movie) => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   )
 }
