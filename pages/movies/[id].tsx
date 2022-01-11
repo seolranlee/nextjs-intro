f59dc093d9990d9dca6fffa61d97e19b1124bbbb
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2022-01-11 18:41:25 +0900

    #2.5 Dynamic Routes

---
+++ pages/movies/[id].tsx
@@ -0,0 +1,9 @@
+// /movies/:movieId
+
+import { useRouter } from "next/router"
+
+export default function Detail() {
+  const router = useRouter()
+  console.log(router.query.id)
+  return "detail"
+}
